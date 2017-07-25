83c6d3136a2c9e5eb87f4914bc908e0c619b5a25
Jonathan Alzate <jalzate@neurala.com>
2017-07-24 23:45:00 -0400

    revised model version files uploaded

---
+++ final_writeup.md
@@ -0,0 +1,131 @@
+#**Behavioral Cloning** 
+
+---
+
+[image1]: ./report_img/title_image.png "Behavioral Cloning"
+
+**Behavioral Cloning Project**
+
+The goals / steps of this project are the following:
+* Use the simulator to collect data of good driving behavior
+* Build, a convolution neural network in Keras that predicts steering angles from images
+* Train and validate the model with a training and validation set
+* Test that the model successfully drives around track one without leaving the road
+* Summarize the results with a written report
+
+
+
+
+
+
+
+## Rubric Points
+###Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/432/view) individually and describe how I addressed each point in my implementation.  
+
+---
+###Files Submitted & Code Quality
+
+####1. Submission includes all required files and can be used to run the simulator in autonomous mode
+
+My project includes the following files:
+* model.py containing the script to create and train the model
+* drive.py for driving the car in autonomous mode
+* model.h5 containing a trained convolution neural network 
+* writeup_report.md summarizing the results
+
+####2. Submission includes functional code
+Using the Udacity provided simulator and my drive.py file, the car can be driven autonomously around the track by executing 
+```sh
+python drive.py model.h5
+```
+
+####3. Submission code is usable and readable
+
+The model.py file contains the code for training and saving the convolution neural network. The file shows the pipeline used for training and validating the model, and it contains comments to explain how the code works.
+
+
+
+###Model Architecture and Training Strategy
+
+####1. An appropriate model architecture has been employed
+
+I decided to implement the End-to-End model from NVIDIA because it seemed to be the most adequate for the task, in my personal opinion. The model has the following architecture, from model.summary()
+
+
+
+####2. Attempts to reduce overfitting in the model
+ 
+The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 28). 
+The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
+
+####3. Model parameter tuning
+
+The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 51).
+
+####4. Appropriate training data
+
+Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road on both tracks
+to generalize the data. 
+
+For details about how I created the training data, see the next section. 
+
+
+
+###Model Architecture and Training Strategy
+
+####1. Solution Design Approach
+
+The overall strategy for deriving a model architecture was to choose from available models learned or referenced in class the model that seem best suited for the task at hand. The NVIDIA
+model definitely seem the better choice, it included convolutional layers and different activation functions so I decided to go with this model architecture.
+
+Considering computation limitation I decided to have as much processing happening on the GPU so the first layer of my model was replaced with a Cropping2D layer to remove trees and skyline from
+images from the top and the hood of the car from the bottom. The second layer is a Lambda layer that takes care of the normalization step. Another step to reduce computation on CPU was to use 
+the Generator class from Keras in order to have all the image preprocessing (image flipping, shifting, color change, etc) happening on the GPU.
+
+The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track may be due to left/right turn bias.
+To improve the driving behavior in these cases, I recorded a couple of training runs from both tracks and used images from all three cameras to have the model trained in more data
+
+At the end of the process, the vehicle is able to drive autonomously around the track without leaving the road.
+
+####2. Final Model Architecture
+
+The final model architecture (model.py lines 31-46) consisted of a convolution neural network with the following layers and layer sizes ...
+
+Here is a visualization of the architecture (note: visualizing the architecture is optional according to the project rubric)
+
+[image2]: ./model_summary.png "Model Summary"
+
+
+####3. Creation of the Training Set & Training Process
+
+To capture good driving behavior, I first recorded three laps in both directions (clockwise, counterclockwise) on track one using center lane driving. Here is an example image of center lane driving:
+
+[image3]: ./report_img/left.png "Left Image"
+[image4]: ./report_img/right.png "Right Image"
+[image5]: ./report_img/center.png "Center Image"
+
+
+I then recorded the vehicle recovering from the left side and right sides of the road back to center so that the vehicle would learn to adjust itself back on to the center
+
+Then I repeated this process on track two in order to get more data points.
+
+To augment the data set, I also flipped images and angles thinking that this would compensate for any left/right turn bias, when not using all three cameras.
+I also gathered driving data from track two, three laps in each direction clockwise and counterclockwise to compensate any turn bias. 
+
+After the collection process, I then preprocessed this data by doing the following:
+
+* flipping
+* convert to rgb
+* crop
+* normalized
+
+
+I finally randomly shuffled the data set and split data into a training and validation set. 
+
+I used this training data for training the model. The validation set helped determine if the model was over or under fitting. 
+The ideal number of epochs was 7-10 as evidenced by the loss and validation loss not having any improvements after 10 epochs
+I used an adam optimizer so that manually training the learning rate wasn't necessary, but I still left the option open to set a tunable learning rate.
+
+The loss and validation loss plot after training 
+
+[image6]: ./report_img/loss_plot.png "Left Image"
---
+++ model.py
@@ -0,0 +1,159 @@
+# Import libraries necessary for this project.
+import json
+import numpy as np
+import pandas as pd
+import cv2
+from keras.models import Sequential
+from keras.layers import Activation, Conv2D, Dense, Flatten, Lambda
+from keras.layers.convolutional import Convolution2D, Cropping2D
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard, CSVLogger
+from PIL import Image
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# Location of the simulator data.
+DATA_FILE = './driving_data/driving_log.csv'
+local_img_path = './driving_data/IMG/'
+
+# Load the training data from the simulator.
+cols = ['Center Image', 'Left Image', 'Right Image', 'Steering Angle', 'Throttle', 'Break', 'Speed']
+data = pd.read_csv(DATA_FILE, names=cols, header=1)
+
+# Separate the image paths and steering angles.
+images = data[['Center Image', 'Left Image', 'Right Image']]
+angles = data['Steering Angle']
+
+# Split the data into training and validation sets.
+images_train, images_validation, angles_train, angles_validation = train_test_split(images, angles, test_size=0.15, random_state=42)
+
+# Define the model
+model = Sequential()
+# cropping top and bottom of image remove trees and sky
+model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+# Normalization layer
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Convolution2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+model.add(Flatten())
+model.add(Dense(1164, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1, activation='tanh'))
+
+# Print out summary of the model
+model.summary()
+# Select the optimizer and compile the model.
+optimizer = Adam(lr=0.0001)
+model.compile(loss='mse', optimizer=optimizer)
+
+# Helper to load an normalize images.
+def load_image(path, flip=False):
+    # get local path to file
+    filename = path.split('/')[-1]
+    local_path = local_img_path + filename
+    
+    # Read the image from disk, and flip it if requested.
+    image = cv2.imread(local_path)
+    if flip:
+        image = cv2.flip(image, 1)
+    
+    # Return the normalized image.
+    return image
+
+# Data generator.
+def generate_batches(images, angles, batch_size=64, augment=True):
+    # Create an array of sample indexes.
+    indexes = np.arange(len(images))
+    batch_images = []
+    batch_angles = []
+    sample_index = 0
+    while True:
+        # Reshuffle the indexes after each pass through the samples to minimize
+        # overfitting on the data.
+        np.random.shuffle(indexes)
+        for i in indexes:
+            # Increment the number of samples. 
+            sample_index += 1
+            
+            # Load the center image and weight.
+            center_image = load_image(images.iloc[i]['Center Image'])
+            center_angle = float(angles.iloc[i])
+            batch_images.append(center_image)
+            batch_angles.append(center_angle)
+            
+            # Add augmentation if requested
+            if augment:
+                # Load the flipped image and invert angle
+                flipped_image = load_image(images.iloc[i]['Center Image'], True)
+                flipped_angle = -1. * center_angle
+                batch_images.append(flipped_image)
+                batch_angles.append(flipped_angle)
+
+                # Load the left image and adjust angle
+                left_image = load_image(images.iloc[i]['Left Image'])
+                left_angle = min(1.0, center_angle + 0.25)
+                batch_images.append(left_image)
+                batch_angles.append(left_angle)
+                # Load the right image and adjust angle
+                right_image = load_image(images.iloc[i]['Right Image'])
+                right_angle = max(-1.0, center_angle - 0.25)
+                batch_images.append(right_image)
+                batch_angles.append(right_angle)
+            
+            # If we have processed batch_size samples or this is the last batch
+            # of the epoch, then submit the batch. Note that due to augmentation
+            # there may be more than batch_size elements in the batch.
+            if (sample_index % batch_size) == 0 or (sample_index % len(images)) == 0:
+                yield np.array(batch_images), np.array(batch_angles)
+                batch_images = []
+                batch_angles = []
+
+# Instantiate data generators for training and validation.
+nb_epoch = 10
+samples_per_epoch = 4 * len(images_train)
+generator_train = generate_batches(images_train, angles_train)
+nb_val_samples = len(images_validation)
+generator_validation = generate_batches(images_validation, angles_validation, augment=False)
+
+# Generator callbacks (TensorBoard, CSVLogger)
+tensorboard = TensorBoard(log_dir='./model_tensorboard', histogram_freq=1, 
+                          write_graph=True, write_images=True)
+
+csv_logger = CSVLogger('model_log.csv', append=True, separator=';')
+
+callbacksList = [tensorboard, csv_logger]
+
+# Run the model.
+history = model.fit_generator(
+    generator_train, samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch,
+    validation_data=generator_validation, nb_val_samples=nb_val_samples, callbacks=callbacksList)
+
+# plot model and save to file
+#plot(model, to_file='model_nvidia.png')
+model.save('model.h5')
+
+# Save the generated model and weights.
+# Save model as json file
+json_string = model.to_json()
+
+with open('model.json', 'w') as outfile:
+    json.dump(json_string, outfile)
+    
+    # save weights
+    model.save_weights('./model_weights.h5')
+    print("Saved")
+    
+
+# plot model
+plt.plot(history.history(['loss']))
+plt.plot(history.history(['val_loss']))
+plt.title('Model Mean-Squared Error Loss')
+plt.ylabel('Mean-Squared Error Loss')
+plt.xlabel('Epoch')
+plt.legend(['Training Set', 'Validation Set'], loc='upper right')
+plt.savefig('model_plot')
